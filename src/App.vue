d9db91b7b7f597dda464425428260ecd3497da3a
MageeLin <linjingyi321@qq.com>
2021-07-24 13:50:56 +0800

    chore: 使用element-plus框架

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <router-link to="/">Home</router-link> |
+  <router-link to="/about">About</router-link>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
-<style lang="scss">
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  margin-top: 60px;
 }
 </style>
